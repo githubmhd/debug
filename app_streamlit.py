a429d61b6b816e8bab8492327fc6b810ed9dab24
githubmhd <contact.mohamedh@gmail.com>
2024-06-10 17:02:05 +0200

    test

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import seaborn as sns
-url = 'https://github.com/githubmhd/movie_recommendations/blob/main/App/movie_final_ML.csv'
+url = 'https://raw.githubusercontent.com/githubmhd/movie_recommendations/main/App/movie_final_ML.csv'
 st.title('Build and shared data application!')
 with st.container():
     st.write("This is inside the first container")
